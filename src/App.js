d55c055d1460db266a337cb021c7387bfcf9fa2d
Pedro Salazar Paredes <pdrslzr@gmail.com>
2019-09-17 13:29:34 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+import navio from "navio";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "https://www.datos.gov.co/resource/r4q3-ept4.json"
+    }
+  }
+
+
+
+  componentDidMount() {
+   this.navigate();
+  }
+
+  navigate() {
+    this.nv = new navio(this.Navegante, 600);
+    fetch(this.state.inputValue)
+    .then(data => this.nv.data(data))
+    .then(this.nv.addAllAttribs());
+  }
+
+  
+
+  renderTweets() {
+    //return this.state.tweets.map(t => <Tweet key={t.id} tweet={t}></Tweet>);
+  }
+
+  onChange() {
+    this.setState({
+      inputValue: this.APIentrada.value
+    })
+    console.log(this.APIentrada.value)
+  }
+
+  render() {
+    return (
+    <div className="App">
+     <input type="text" defaultValue={this.state.inputValue} ref={APIentrada => this.APIentrada = APIentrada} onInput = {this.onChange.bind(this)}/>
+     <div ref={Navegante => this.Navegante = Navegante}></div>
+    </div>);
+  }
+}
+
+export default App;
