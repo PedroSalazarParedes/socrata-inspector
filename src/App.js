34cfa9c3a48e86353e66eb3771dfe0e96d7ee17a
Pedro Salazar Paredes <pdrslzr@gmail.com>
2019-09-17 19:02:12 -0500

    gbuysidnx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, {Component} from "react";
 import navio from "navio";
+import "./navegante.css";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: "https://www.datos.gov.co/resource/r4q3-ept4.json"
+      isLoading: true,
+      inputValue: "https://www.datos.gov.co/resource/nsxu-h2dh.json"
     }
   }
 
-
+  async loadDataNicely(url) {
+    let loaded = false;
+    let daticos = [];
+    while(!loaded) {
+      let output = await fetch(`${url}?$limit=10000&$offset=${daticos.length}`).then(res => res.json());
+      if(output.length !== 0) {
+        daticos = daticos.concat(output);
+      } else {
+        loaded = true;
+      }
+    }
+    return daticos;
+  }
 
   componentDidMount() {
-   this.navigate();
+   this.navigateWithPages();
   }
 
   componentDidUpdate() {
-    this.navigate();
+    this.navigateWithPages();
   }
 
   navigate() {
@@ -25,27 +39,62 @@ class App extends Component {
     fetch(this.state.inputValue)
     .then(data => data.json())
     .then(datos => this.nv.data(datos))
-    .then(() => this.nv.addAllAttribs());
+    .then(() => this.nv.addAllAttribs())
+    .catch(() => console.log("Fallamos amigos"));
   }
 
-  
-
-  renderTweets() {
-    //return this.state.tweets.map(t => <Tweet key={t.id} tweet={t}></Tweet>);
+  async navigateWithPages() {
+    this.nv = new navio(this.Navegante, 600);
+    this.loadDataNicely(this.state.inputValue)
+    .then(datos => this.nv.data(datos))
+    .then(() => {
+      this.nv.addAllAttribs();
+    })
+    .catch(err => console.log(err,"Fallamos amigos"));
   }
 
-  onChange() {
+  feelingLucky() {
+    const item =  Math.floor(Math.random() * (10));
+
+    const apis = ["https://www.datos.gov.co/resource/vwwf-4ftk.json",
+     "https://www.datos.gov.co/resource/54ah-2npf.json",
+    "https://www.datos.gov.co/resource/6du3-6rbr.json",
+    "https://www.datos.gov.co/resource/7pcy-5vx9.json",
+    "https://www.datos.gov.co/resource/fcsx-656w.json",
+    "https://www.datos.gov.co/resource/m6r5-5g2i.json",
+    "https://www.datos.gov.co/resource/nsxu-h2dh.json",
+    "https://www.datos.gov.co/resource/kcdt-jbvj.json",
+    "https://www.datos.gov.co/resource/sdmr-tfmf.json",
+    "https://www.datos.gov.co/resource/rvhf-aw6a.json"
+  ];
+  const api = apis[item];
+    this.APIentrada.value=api;
+    this.setState({
+      inputValue: api
+    })
+  }
+
+  onLoad() {
     this.setState({
       inputValue: this.APIentrada.value
     })
-    console.log(this.APIentrada.value)
   }
 
   render() {
     return (
     <div className="App">
-     <input type="text" defaultValue={this.state.inputValue} ref={APIentrada => this.APIentrada = APIentrada} onInput = {this.onChange.bind(this)}/>
-     <div ref={Navegante => this.Navegante = Navegante}></div>
+      <div className="input-group">
+        <input type="text" className="form-control" defaultValue={this.state.inputValue} ref={APIentrada => this.APIentrada = APIentrada}/>
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="button" onClick={this.onLoad.bind(this)}>Load dataset</button>
+          <button className="btn btn-outline-secondary" type="button" onClick={this.feelingLucky.bind(this)}>
+            <span role="img" aria-label="emojis">
+              🤩🚀🦕
+            </span>
+          </button>
+        </div>
+      </div>
+      <div className="navegante" ref={Navegante => this.Navegante = Navegante}></div>
     </div>);
   }
 }
